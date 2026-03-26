29b84223a10fcbb822e48f2b76d0491d6943a7d0
Jordan <jordan.marshall1@mohawkcollege.ca>
2026-03-25 23:40:19 -0400

    Add player class & scene class

---
+++ team-mjs-a4-interrogation-game/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace MohawkGame2D
+{
+    public class Player
+    {
+        public Vector2 playerPosition = new Vector2(400, 200);
+        public float playerRadius = 25;
+        public float detectRadius = 50;
+
+        public void PlayerMove()
+        {
+            // Setup
+            Draw.FillColor = Color.LightGray;
+            Draw.Circle(playerPosition, playerRadius);
+
+            // Movement
+            if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                playerPosition.X -= 5;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                playerPosition.X += 5;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                playerPosition.Y -= 5;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.S))
+            {
+                playerPosition.Y += 5;
+            }
+
+            // Collision
+            if (playerPosition.Y - playerRadius < 0)
+            {
+                playerPosition.Y = 25;
+            }
+            if (playerPosition.Y + playerRadius > 720)
+            {
+                playerPosition.Y = 695;
+            }
+            if (playerPosition.X - playerRadius < 0)
+            {
+                playerPosition.X = 25;
+            }
+            if (playerPosition.X + playerRadius > 1280)
+            {
+                playerPosition.X = 1255;
+            }
+        }
+    }
+
+}

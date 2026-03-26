74d95e0eae600aa3213da4d32a19be72cc4e4c58
Jordan <jordan.marshall1@mohawkcollege.ca>
2026-03-25 22:07:21 -0400

    Add player

--- team-mjs-a4-interrogation-game/Game.cs
+++ team-mjs-a4-interrogation-game/Game.cs
@@ -8,18 +8,64 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        Vector2 playerPosition = new Vector2(400, 200);
+        float playerRadius = 25;
 
         public void Setup()
         {
-            Window.SetTitle("Interrogation");
-            Window.SetSize(1280, 720);
+            Window.SetSize(800, 800);
+            Window.SetTitle("Interrogation Game");
+            Window.TargetFPS = 60;
         }
 
         public void Update()
         {
             Window.ClearBackground(Color.Black);
+            Player();
+
+        }
+
+        public void Player()
+        {
+            // Setup
+            Draw.FillColor = Color.LightGray;
+            Draw.Circle(playerPosition, playerRadius);
+
+            // Movement
+            if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                playerPosition.X -= 5;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                playerPosition.X += 5;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                playerPosition.Y -= 5;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.S))
+            {
+                playerPosition.Y += 5;
+            }
+
+            // Collision
+            if (playerPosition.Y - playerRadius < 0)
+            {
+                playerPosition.Y = 25;
+            }
+            if (playerPosition.Y + playerRadius > 800)
+            {
+                playerPosition.Y = 775;
+            }
+            if (playerPosition.X - playerRadius < 0)
+            {
+                playerPosition.X = 25;
+            }
+            if (playerPosition.X + playerRadius > 800)
+            {
+                playerPosition.X = 775;
+            }
         }
     }
-
 }
